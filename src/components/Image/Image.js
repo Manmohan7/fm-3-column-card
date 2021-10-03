cfbe9e0edda4c6befe16fa2f4dd39791480fa1ca
Manmohan Singh <er.manmohansingh7@gmail.com>
2021-10-03 22:51:56 +0530

    image component

---
+++ src/components/Image/Image.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Image = ({ title, path }) => {
+  const images = require.context('../../../public/images', true);
+  const src = images(path).default;
+
+  return (
+    <img src={src} alt={title} className="h-10 lg:h-10" />
+  )
+}
+
+Image.propTypes = {
+  path: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired
+};
+
+export default Image;
